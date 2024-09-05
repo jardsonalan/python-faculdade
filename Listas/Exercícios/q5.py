f98da39a3955fc6b006fa3171cc9a95a2e885ade
jardsonalan <jardsonalan132@gmail.com>
2024-09-05 07:44:23 -0300

    update: questão 5 - resolvida

--- Listas/Exercícios/q5.py
+++ Listas/Exercícios/q5.py
@@ -1,33 +1,36 @@
-i = 0
-
-maiorPar = []
-menorImpar = []
+par = [0]
+impar = []
 listaCompleta = []
 
-while i < 5:
-    numero = int(input(f"Informe o {i}° número inteiro: "))
+for i in range(5):
+    numero = int(input(f"Informe o {i+1}° número inteiro: "))
     listaCompleta.append(numero)
 
     if numero % 2 == 0:
-        maiorPar.append(numero)
-        if numero > maiorPar[0]:
-            maiorPar.pop(0)
-            maiorPar.append(numero)
+        par.append(numero)
+        
     elif numero % 2 != 0:
-        menorImpar.append(numero)
-        if numero < menorImpar[0]:
-            menorImpar.pop(0)
-            menorImpar.append(numero)
-    
+        impar.append(numero)
+
+maiorPar=0
+i=0
+while i < len(par):
+    if par[i] > maiorPar:
+        maiorPar = par[i]
+    else:
+        pass
     i+=1
+
+impar.sort()
+menorImpar = impar[0]
 
 i=0
-while i < 5:
-    elemento = listaCompleta[i]
-    somaElementos = elemento + elemento
+soma=0
+while i < len(listaCompleta):
+    soma+=listaCompleta[i]
     i+=1
 
-print(f'A soma dos elementos é igual a: {somaElementos}')
-print(f'A média é igual: {somaElementos/2}')
-print(f'O maior par é: {maiorPar[0]}')
-print(f'O menor ímpar é: {menorImpar[0]}')
+print(f'A soma dos elementos é igual a: {soma}')
+print(f'A média é igual: {soma/2}')
+print(f'O maior par é: {maiorPar}')
+print(f'O menor ímpar é: {menorImpar}')
