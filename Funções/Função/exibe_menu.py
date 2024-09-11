8a3a3837667b62f9ba7a7f49aa081b0702ba1bc2
jardsonalan <jardsonalan132@gmail.com>
2024-09-11 10:18:51 -0300

    update: função de subtrair adicionada ao exercício

--- Funções/Função/exibe_menu.py
+++ Funções/Função/exibe_menu.py
@@ -2,11 +2,16 @@ def exibeMenu():
     print('#### MENU ####\n')
     print('0 - SAIR')
     print('1 - SOMAR')
+    print('2 - SUBTRAIR')
     opcao = int(input('Escolha uma opção: '))
     return opcao
 
 def somar(numero1, numero2):
     resultado = numero1+numero2
+    return resultado
+
+def subtrair(numero1, numero2):
+    resultado = numero1-numero2
     return resultado
 
 opcao=1
@@ -24,4 +29,8 @@ while opcao != 0:
 
     if opcao == 1:
         resultado = somar(num1, num2)
+
+    elif opcao == 2:
+        resultado = subtrair(num1, num2)
+
     print(f'Resultado: {resultado}')
